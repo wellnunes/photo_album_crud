d855fa5b2ece90fdbe4c144426b3eb9f1033f53c
Wellisson Nunes <wellissonln@gmail.com>
2023-01-20 12:04:39 -0300

    fix: fields names fixed in Photo model

--- photos/models.py
+++ photos/models.py
@@ -5,5 +5,5 @@ class Photo(models.Model):
     name = models.CharField('Nome da foto', max_length=100)
     place = models.CharField('Local da foto', max_length=100)
     date = models.DateField('Data da foto')
-    photographer = models.DateField('Local da foto', max_length=100)
+    photographer = models.CharField('Nome do fotógrafo', max_length=100)
     camera_flash = models.BooleanField('Flash da Câmera')
