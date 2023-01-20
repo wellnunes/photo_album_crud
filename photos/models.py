1a96b483393a36cec12643f216fced9cdd143f4c
Wellisson Nunes <wellissonln@gmail.com>
2023-01-20 12:02:08 -0300

    feat: initial view ans serializer created

--- photos/models.py
+++ photos/models.py
@@ -7,7 +7,3 @@ class Photo(models.Model):
     date = models.DateField('Data da foto')
     photographer = models.DateField('Local da foto', max_length=100)
     camera_flash = models.BooleanField('Flash da Câmera')
-
-    # timestamps
-    created_at = models.DateTimeField('Data de criação', auto_now_add=True)
-    updated_at = models.DateTimeField('Data de atualização', auto_now=True)
--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from photos.models import Photo
+from photos.serializers import PhotoSerializer
+
+
+class PhotoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+    permission_classes = [permissions.AllowAny]
