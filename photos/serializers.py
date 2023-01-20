917c1a2e5a9f7204e96dfaf9a3ee2ccc04cbebc1
Wellisson Nunes <wellissonln@gmail.com>
2023-01-20 12:48:59 -0300

    feat: filters config. and serializer id included

--- photos/serializers.py
+++ photos/serializers.py
@@ -6,4 +6,4 @@ from photos.models import Photo
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
-        fields = ['name', 'place', 'date', 'photographer', 'camera_flash']
+        fields = ['id', 'name', 'place', 'date', 'photographer', 'camera_flash']
--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,4 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions, generics
 
 from photos.models import Photo
 from photos.serializers import PhotoSerializer
@@ -13,3 +11,29 @@ class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
     permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Photo.objects.all()
+        name = self.request.query_params.get('name')
+        place = self.request.query_params.get('place')
+        initial_date = self.request.query_params.get('initial_date')
+        final_date = self.request.query_params.get('initial_date')
+        photographer = self.request.query_params.get('photographer')
+        camera_flash = self.request.query_params.get('camera_flash')
+
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        if place is not None:
+            queryset = queryset.filter(place__icontains=place)
+        if initial_date and final_date:
+            queryset = queryset.filter(date__range=(initial_date, final_date))
+        if photographer is not None:
+            queryset = queryset.filter(photographer__icontains=photographer)
+        if camera_flash is not None:
+            queryset = queryset.filter(camera_flash=camera_flash)
+
+        return queryset
